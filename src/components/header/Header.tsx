50ec127e0fd2a61884bec65b9ac2c604757d28f5
Nikita Samoylov <nsam7655@gmail.com>
2024-06-15 00:38:34 +0500

    feat/add account users popup

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import HeaderPopup from '../header-popup/HeaderPopup';
 import styles from './Header.module.scss';
 
 import AvatarImg from './avatar.png';
@@ -5,6 +7,11 @@ import ChatImg from './chat-icon.png';
 import ArrowImg from './Path.png';
 
 const Header: React.FC = () => {
+  const [isModal, setIsModal] = useState(false);
+
+  const handleModal = () => {
+    setIsModal(isModal => !isModal)
+  };
 
   return (
     <div className={ styles.header }>
@@ -12,13 +19,22 @@ const Header: React.FC = () => {
         <button className={ styles.header__icon }>
           <img src={ ChatImg } alt="чат" />
         </button>
-        <button className={ styles.header__icon_user }>
+        <button className={ styles.header__icon_user }
+          onClick={ handleModal }
+        >
           <img src={ AvatarImg } alt="пользователь" />
           <img src={ ArrowImg } alt="меню"
             className={ styles.header__icon_arrow }
           />
         </button>
       </div>
+      {
+        isModal && (
+          <HeaderPopup
+            handleModal={ handleModal }
+          />
+        )
+      }
     </div>
   )
 };
